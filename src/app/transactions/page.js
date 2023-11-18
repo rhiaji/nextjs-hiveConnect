fbb91d1fa599f7953ee2b00da745e0a82623265f
rhiaji <mrdavepmariano@gmail.com>
2023-11-18 09:35:55 +0800

    first commit

---
+++ src/app/transactions/page.js
@@ -0,0 +1,10 @@
+import Navigation from '../../components/navigation'
+
+export default function CustomPage() {
+    return (
+        <body>
+            <Navigation />
+            <h1>welcome to Transactions</h1>
+        </body>
+    )
+}
